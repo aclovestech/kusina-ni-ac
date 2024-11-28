1bbfab201e6b7c0c5a3a4a5963ec111db8cb4e03
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-11-28 15:55:34 -0800

    chore: remove serializeUser and deserializeUser methods from passport-config.js

--- utils/passport-config.js
+++ utils/passport-config.js
@@ -49,26 +49,4 @@ passport.use(
   )
 );
 
-passport.serializeUser((user, cb) => {
-  console.log(user);
-  cb(null, user);
-});
-
-passport.deserializeUser(async (id, cb) => {
-  try {
-    const query = {
-      text: "SELECT customer_id, email, FROM customers.customers WHERE customer_id = $1",
-      values: [id],
-    };
-
-    const queryResult = await db.query(query);
-
-    if (queryResult.rowCount !== 0) {
-      return cb(null, queryResult.rows[0]);
-    }
-  } catch (err) {
-    cb(err);
-  }
-});
-
 module.exports = passport;
