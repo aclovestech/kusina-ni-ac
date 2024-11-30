bbcefb7ac7737d72042c518fa295352055e5b987
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-11-29 17:33:12 -0800

    refactor: improve error handling in jwt.js using HttpError class and add comments for clarity

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,19 +5,23 @@ const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
 dotenv.config({ path: envFile });
 // JSON Web Token
 const jwt = require("jsonwebtoken");
+// HttpError
+const HttpError = require("../utils/HttpError");
 
+// Generates a JSON Web Token
 const generateAccessToken = (user) => {
-  return jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: "1 hour" });
+  return jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
 };
 
+// Verifies a JSON Web Token
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) throw new HttpError("Unauthorized", 401);
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) throw new HttpError("Invalid Bearer Token", 403);
     req.user = user;
     next();
   });
