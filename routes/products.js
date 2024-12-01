9959bfdb92a3ba6ad68f4ed202f81088267d9f9d
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-11-30 21:31:51 -0800

    feat(products.js): add user authorization checks for product routes to enhance security refactor(products.js): extract user authorization logic into separate functions for better readability and maintainability

--- routes/products.js
+++ routes/products.js
@@ -46,93 +46,93 @@ productsRouter.get("/", async (req, res, next) => {
 });
 
 // Adds a product to the database
-productsRouter.post("/", jwt.authenticateToken, async (req, res, next) => {
-  // Get the role_id from the JWT
-  const { role_name, user_id } = req.user;
+productsRouter.post(
+  "/",
+  jwt.authenticateToken,
+  checkUserAuthorization,
+  async (req, res, next) => {
+    // Get the role_id from the JWT
+    const { user_id } = req.user;
 
-  // Throw an error if the user is not a seller
-  if (role_name !== "Seller" && role_name !== "Admin") {
-    throw new Error("Unauthorized", 401);
-  }
+    // Specify joi schema
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      price: Joi.number().required(),
+      stock_quantity: Joi.number().required(),
+      category_id: Joi.number().required(),
+    });
 
-  // Specify joi schema
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    description: Joi.string().required(),
-    price: Joi.number().required(),
-    stock_quantity: Joi.number().required(),
-    category_id: Joi.number().required(),
-  });
+    // Validate the input
+    const { value, error } = schema.validate(req.body);
 
-  // Validate the input
-  const { value, error } = schema.validate(req.body);
+    // Throw an error if there's an error
+    if (error) {
+      throw new HttpError("Missing required data", 400);
+    }
 
-  // Throw an error if there's an error
-  if (error) {
-    throw new HttpError("Missing required data", 400);
-  }
-
-  // These two variables will be used in our queries
-  let query;
-  let queryResponse;
-
-  // Query: Check all categories
-  query = `SELECT * FROM products.categories`;
-  // Response
-  queryResponse = await db.query(query);
-
-  // Validate if the category from the input
-  const filteredArray = queryResponse.rows.filter((row) => {
-    return row.category_id === value.category_id;
-  });
-
-  // Throw an error if the given category ID is not found
-  if (filteredArray.length === 0) {
-    throw new HttpError("Invalid category ID", 400);
-  }
-
-  // Establish a new connection to the database since we are going to make a transaction
-  const client = await db.getClient();
-
-  try {
-    // Begin transaction
-    await client.query("BEGIN");
+    // These two variables will be used in our queries
+    let query;
+    let queryResponse;
 
     // Query: Check all categories
-    query = `
+    query = `SELECT * FROM products.categories`;
+    // Response
+    queryResponse = await db.query(query);
+
+    // Validate if the category from the input
+    const filteredArray = queryResponse.rows.filter((row) => {
+      return row.category_id === value.category_id;
+    });
+
+    // Throw an error if the given category ID is not found
+    if (filteredArray.length === 0) {
+      throw new HttpError("Invalid category ID", 400);
+    }
+
+    // Establish a new connection to the database since we are going to make a transaction
+    const client = await db.getClient();
+
+    try {
+      // Begin transaction
+      await client.query("BEGIN");
+
+      // Query: Check all categories
+      query = `
       INSERT INTO products.products(seller_id, category_id, name, description, price, stock_quantity)
       VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING *
       `;
-    // Response
-    queryResponse = await client.query(query, [
-      user_id,
-      value.category_id,
-      value.name,
-      value.description,
-      value.price,
-      value.stock_quantity,
-    ]);
+      // Response
+      queryResponse = await client.query(query, [
+        user_id,
+        value.category_id,
+        value.name,
+        value.description,
+        value.price,
+        value.stock_quantity,
+      ]);
 
-    // Commit the transaction if all queries we successful
-    await client.query("COMMIT");
-  } catch (err) {
-    // Rollback the transaction if a query was unsuccessful
-    await client.query("ROLLBACK");
+      // Commit the transaction if all queries we successful
+      await client.query("COMMIT");
+    } catch (err) {
+      // Rollback the transaction if a query was unsuccessful
+      await client.query("ROLLBACK");
 
-    // Throw an error since the transaction was unsuccessful
-    console.error(`Transaction error: ${err.message}`);
-    err.message = "Invalid request";
-    err.statusCode = 400;
-    throw err;
-  } finally {
-    // Release the connection after doing the transaction
-    client.release();
+      // Throw an error since the transaction was unsuccessful
+      console.error(`Transaction error: ${err.message}`);
+      err.message = "Invalid request";
+      err.statusCode = 400;
+      throw err;
+    } finally {
+      // Release the connection after doing the transaction
+      client.release();
+    }
+
+    // Return the newly created product
+    res.status(201).json(queryResponse.rows[0]);
   }
-
-  // Return the newly created product
-  res.status(201).json(queryResponse.rows[0]);
-});
+);
 
 // Provides the details of a specific product
 productsRouter.get("/:productId", async (req, res, next) => {
@@ -162,6 +162,11 @@ productsRouter.get("/:productId", async (req, res, next) => {
   // Response
   queryResponse = await db.query(query, [value.product_id]);
 
+  // Throw an error if the product is not found
+  if (queryResponse.rows.length === 0) {
+    throw new HttpError("Product not found", 404);
+  }
+
   // Return the products
   res.status(200).json(queryResponse.rows);
 });
@@ -170,6 +175,7 @@ productsRouter.get("/:productId", async (req, res, next) => {
 productsRouter.put(
   "/:productId",
   jwt.authenticateToken,
+  checkUserAuthorization,
   validateSellerProduct,
   async (req, res, next) => {
     // Specify joi schema
@@ -243,18 +249,19 @@ productsRouter.put(
 productsRouter.delete(
   "/:productId",
   jwt.authenticateToken,
+  checkUserAuthorization,
   validateSellerProduct,
   async (req, res, next) => {
     // Establish a new connection to the database since we are going to make a transaction
     const client = await db.getClient();
 
-    // These two variables will be used in our queries
-    let query;
-    let queryResponse;
-
     try {
       // Begin transaction
       await client.query("BEGIN");
+
+      // These two variables will be used in our queries
+      let query;
+      let queryResponse;
 
       // Query: Delete the product
       query = `
@@ -287,9 +294,24 @@ productsRouter.delete(
   }
 );
 
-async function validateSellerProduct(req, res, next) {
+// Checks if the user is a seller or an admin
+function checkUserAuthorization(req, res, next) {
   // Get the role_id from the JWT
-  const { role_name, user_id } = req.user;
+  const { role_name } = req.user;
+
+  // Throw an error if the user is not a seller
+  if (role_name !== "Seller" && role_name !== "Admin") {
+    throw new HttpError("Unauthorized", 401);
+  }
+
+  // Move to the next middleware
+  next();
+}
+
+// Checks if the user is the seller of the product
+async function validateSellerProduct(req, res, next) {
+  // Get the user_id from the JWT
+  const { user_id } = req.user;
 
   // These two variables will be used in our queries
   let query;
@@ -311,7 +333,7 @@ async function validateSellerProduct(req, res, next) {
 
   // Throw an error if the user is not a seller, or if the user is not the seller of the product
   const isSeller = queryResponse.rows[0].seller_id === user_id;
-  if ((role_name !== "Seller" && role_name !== "Admin") || !isSeller) {
+  if (!isSeller) {
     throw new HttpError("Unauthorized", 401);
   }
 
