d57d4a2b7b6a6f135db43234070028c627df78e0
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-01 18:35:15 -0800

    feat(db.js): create db.js file to handle database connection and user transactions refactor(auth.js): replace direct database queries with insertUserTransaction for better transaction management refactor(auth.js): add input validation for registration using Joi to ensure data integrity chore(server.js): remove dotenv configuration from server.js as it is now handled in db.js

---
+++ db/db.js
@@ -0,0 +1,70 @@
+// Dotenv
+const dotenv = require("dotenv");
+// HttpError
+const HttpError = require("../utils/HttpError");
+// Determine which .env file to load based on NODE_ENV
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
+dotenv.config({ path: envFile });
+
+// Knex config
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
+});
+
+// Queries
+const insertUserTransaction = async (
+  name,
+  email,
+  hashedPassword,
+  role_id = 3
+) => {
+  try {
+    // Begin Transaction
+    const result = await knex.transaction(async (trx) => {
+      // Insert a row within the users.users table with the required data
+      const [returnedData] = await trx
+        .withSchema("users")
+        .insert([
+          {
+            name: name,
+            email: email,
+            password_hash: hashedPassword,
+            role_id: role_id,
+          },
+        ])
+        .into("users")
+        .returning(["user_id", "email", "created_at"]);
+      // After the user is created, get the returned user_id and insert a row within the customers.customer_details table
+      await trx
+        .withSchema("customers")
+        .insert({ customer_id: returnedData.user_id })
+        .into("customer_details");
+      // After that insert a row within the customers.carts table
+      await trx
+        .withSchema("customers")
+        .insert({ customer_id: returnedData.user_id })
+        .into("carts");
+
+      // Return back the user_id, email, and created_at
+      return returnedData;
+    });
+
+    // Return the result if the transaction was successful
+    return result;
+  } catch (err) {
+    // Throw an error since the transaction was unsuccessful
+    console.error(`Transaction error: ${err.message}`);
+    throw new HttpError();
+  }
+};
+
+module.exports = {
+  insertUserTransaction,
+};
