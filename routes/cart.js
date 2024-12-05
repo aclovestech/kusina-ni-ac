cb3b24c9ae1f6c734d9ae86d2c65d1d7bffa9386
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-04 22:59:06 -0800

    feat(cart.js): add base cart management routes for creating a new cart instance, getting the cart details, adding, updating, and deleting items in a cart to enhance cart functionality

--- routes/cart.js
+++ routes/cart.js
@@ -3,4 +3,22 @@ const Router = require("express-promise-router");
 
 const cartRouter = new Router();
 
+// Creates a new cart
+cartRouter.post("/", async (req, res, next) => {});
+
+// Gets a specific cart
+cartRouter.get("/:cartId", async (req, res, next) => {});
+
+// Adds an item to a specific cart
+cartRouter.post("/:cartId", async (req, res, next) => {});
+
+// Updates the quantity of an item in a specific cart
+cartRouter.put("/:cartId/:productId", async (req, res, next) => {});
+
+// Deletes an item from a specific cart
+cartRouter.delete("/:cartId/:productId", async (req, res, next) => {});
+
+// Checks out a specific cart
+cartRouter.post("/:cartId/checkout", async (req, res, next) => {});
+
 module.exports = cartRouter;
