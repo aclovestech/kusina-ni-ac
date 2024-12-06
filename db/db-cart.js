0f20870ee70dc29369b89a40b866babbcae9b1ab
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-05 19:20:27 -0800

    feat(cart): add functionality to add items to a cart and validate cart ownership
    - Implement `addItemsToCart` function to handle adding items to a cart.
    - Create `validateCartIdByUserId` function to check if a cart belongs to a user.
    - Update cart routes to include validation for cart ID and input data using Joi.
    - Enhance error handling for invalid input and unauthorized access.

--- db/db-cart.js
+++ db/db-cart.js
@@ -20,4 +20,47 @@ const createNewCart = async (user_id) => {
   }
 };
 
-module.exports = { createNewCart };
+// Adds an item to a specific cart
+const addItemsToCart = async (cart_id, items) => {
+  try {
+    // Query: Add items to the cart
+    const result = await knex
+      .batchInsert(
+        "customers.cart_items",
+        items.map((item) => ({ cart_id: cart_id, ...item }))
+      )
+      .returning("*");
+
+    // Return the data from the response
+    return result;
+  } catch (err) {
+    // Throw an error since the query was unsuccessful
+    console.error(`Query error: ${err.message}`);
+    throw new HttpError();
+  }
+};
+
+// Validates if a specific cart belongs to a specific user
+const validateCartIdByUserId = async (cart_id, user_id) => {
+  try {
+    // Query: Check if the cart belongs to the user
+    const [returnedData] = await knex("customers.carts")
+      .select("cart_id", "customer_id")
+      .where("cart_id", cart_id)
+      .andWhere("customer_id", user_id);
+
+    // Return false if the cart doesn't belong to the user
+    if (!returnedData) {
+      return false;
+    }
+
+    // Otherwise, return true
+    return true;
+  } catch (err) {
+    // Throw an error since the query was unsuccessful
+    console.error(`Query error: ${err.message}`);
+    throw new HttpError();
+  }
+};
+
+module.exports = { createNewCart, addItemsToCart, validateCartIdByUserId };
--- routes/cart.js
+++ routes/cart.js
@@ -2,8 +2,14 @@
 const Router = require("express-promise-router");
 // HttpError
 const HttpError = require("../utils/HttpError");
+// Joi
+const Joi = require("joi");
 // DB (Knex)
-const { createNewCart } = require("../db/db-cart");
+const {
+  createNewCart,
+  addItemsToCart,
+  validateCartIdByUserId,
+} = require("../db/db-cart");
 
 const cartRouter = new Router();
 
@@ -19,8 +25,46 @@ cartRouter.post("/", checkUserAuthorization, async (req, res, next) => {
 // Gets a specific cart
 cartRouter.get("/:cartId", async (req, res, next) => {});
 
-// Adds an item to a specific cart
-cartRouter.post("/:cartId", async (req, res, next) => {});
+// Adds an item/multiple items to a specific cart
+cartRouter.post(
+  "/:cartId",
+  checkUserAuthorization,
+  validateCartIdInput,
+  async (req, res, next) => {
+    // Specify joi schema for a single item
+    const itemSchema = Joi.object(
+      {
+        product_id: Joi.string().uuid().required(),
+        quantity: Joi.number().integer().min(1).required(),
+      },
+      { stripUnknown: true }
+    );
+    // Specify joi schema for inputting all items
+    const itemsSchema = Joi.object(
+      {
+        items: Joi.array().items(itemSchema).min(1).required(),
+      },
+      { stripUnknown: true }
+    );
+
+    // Validate the input
+    const { value, error } = itemsSchema.validate(req.body);
+
+    // Throw an error if there's an error
+    if (error) {
+      throw new HttpError("Invalid input data", 400);
+    }
+
+    // Query: Add items to the cart
+    const result = await addItemsToCart(
+      req.validatedCartId.cart_id,
+      value.items
+    );
+
+    // Return the updated cart
+    res.status(201).json(result);
+  }
+);
 
 // Updates the quantity of an item in a specific cart
 cartRouter.put("/:cartId/:productId", async (req, res, next) => {});
@@ -40,6 +84,39 @@ function checkUserAuthorization(req, res, next) {
   if (role_name !== "Customer") {
     throw new HttpError("Unauthorized", 401);
   }
+
+  // Move to the next middleware
+  next();
+}
+
+function validateCartIdInput(req, res, next) {
+  // Specify joi schema
+  const schema = Joi.object(
+    {
+      cart_id: Joi.string().uuid().required(),
+    },
+    { stripUnknown: true }
+  );
+
+  // Validate the input
+  const { value, error } = schema.validate({
+    cart_id: req.params.cartId,
+  });
+
+  // Throw an error if there's an error
+  if (error) {
+    throw new HttpError("Invalid input data", 400);
+  }
+
+  // Query: Validate the cart ID
+  const isValid = validateCartIdByUserId(value.cart_id, req.user.user_id);
+
+  if (!isValid) {
+    throw new HttpError("Unauthorized", 401);
+  }
+
+  // Save the validated cart ID in the request
+  req.validatedCartId = value;
 
   // Move to the next middleware
   next();
