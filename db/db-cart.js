f53de73d572e708bdf76bf55ecae0bfc1df57e41
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-07 18:46:49 -0800

    feat(cart): refactor cart operations to improve error handling and validation - Simplify error handling by removing try-catch blocks in cart functions - Move validation logic to a dedicated validation module for better organization - Improve code readability and maintainability by restructuring functions and removing redundant code

--- db/db-cart.js
+++ db/db-cart.js
@@ -5,47 +5,74 @@ const HttpError = require("../utils/HttpError");
 
 // Creates a new cart
 const createNewCart = async (user_id) => {
-  try {
-    // Query: Create a new cart
-    const [returnedData] = await knex
-      .insert({ customer_id: user_id }, ["cart_id", "created_at"])
-      .into("customers.carts");
+  // Query: Create a new cart
+  const [returnedData] = await knex("customers.carts").insert(
+    { customer_id: user_id },
+    ["cart_id", "created_at"]
+  );
 
-    // Return the data from the response
-    return returnedData;
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
+  // Return the data from the response
+  return returnedData;
 };
 
 // Adds an item to a specific cart
 const addItemsToCart = async (cart_id, items) => {
-  try {
-    // Query: Add items to the cart
-    const result = await knex
-      .batchInsert(
-        "customers.cart_items",
-        items.map((item) => ({ cart_id: cart_id, ...item }))
-      )
-      .returning("*");
+  // Query: Add items to the cart
+  const result = await knex("customers.cart_items").insert(
+    items.map((item) => ({ cart_id: cart_id, ...item })),
+    ["*"]
+  );
 
-    // Return the data from the response
-    return result;
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
+  // Return the data from the response
+  return result;
 };
 
 // Gets the items in a specific cart
 const getCartItemsByCartId = async (cart_id) => {
-  try {
+  // Query: Get the cart items
+  const result = knex("customers.cart_items")
+    .join("customers.carts", "cart_items.cart_id", "carts.cart_id")
+    .join("products.products", "cart_items.product_id", "products.product_id")
+    .select(
+      "cart_items.quantity",
+      "products.name",
+      "products.description",
+      "products.price",
+      "products.product_id"
+    )
+    .where("cart_items.cart_id", cart_id);
+
+  // Return the data from the response
+  return result;
+};
+
+// Updates the quantity of an item in a specific cart
+const updateCartItemQuantity = async (cart_id, product_id, quantity) => {
+  // Query: Update the cart item quantity
+  const [returnedData] = await knex("customers.cart_items")
+    .update({ quantity: quantity }, ["*"])
+    .where("cart_id", cart_id)
+    .andWhere("product_id", product_id);
+
+  // Return the data from the response
+  return returnedData;
+};
+
+// Deletes an item from a specific cart
+const deleteCartItemByCartIdAndProductId = async (cart_id, product_id) => {
+  // Query: Delete the cart item
+  await knex("customers.cart_items")
+    .del()
+    .where("cart_id", cart_id)
+    .andWhere("product_id", product_id);
+};
+
+// Checkout a specific cart
+const checkoutCart = async (cart_id, customer_id, address_id) => {
+  // Begin transaction
+  const result = await knex.transaction(async (trx) => {
     // Query: Get the cart items
-    const result = knex
-      .from("customers.cart_items")
+    const cartData = await trx("customers.cart_items")
       .join("customers.carts", "cart_items.cart_id", "carts.cart_id")
       .join("products.products", "cart_items.product_id", "products.product_id")
       .select(
@@ -57,136 +84,63 @@ const getCartItemsByCartId = async (cart_id) => {
       )
       .where("cart_items.cart_id", cart_id);
 
-    // Return the data from the response
-    return result;
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
-};
+    // Throw an error if the cart is empty
+    if (cartData.length === 0) {
+      throw new HttpError("Cart is empty", 400);
+    }
 
-// Updates the quantity of an item in a specific cart
-const updateCartItemQuantity = async (cart_id, product_id, quantity) => {
-  try {
-    // Query: Update the cart item quantity
-    const [returnedData] = await knex("customers.cart_items")
-      .update({ quantity: quantity }, ["*"])
-      .where("cart_id", cart_id)
-      .andWhere("product_id", product_id);
-
-    // Return the data from the response
-    return returnedData;
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
-};
-
-// Deletes an item from a specific cart
-const deleteCartItemByCartIdAndProductId = async (cart_id, product_id) => {
-  try {
-    // Query: Delete the cart item
-    await knex("customers.cart_items")
-      .del()
-      .where("cart_id", cart_id)
-      .andWhere("product_id", product_id);
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
-};
-
-// Checkout a specific cart
-const checkoutCart = async (cart_id, customer_id, address_id) => {
-  try {
-    // Begin transaction
-    return await knex.transaction(async (trx) => {
-      // Query: Get the cart items
-      const cartData = await trx("customers.carts")
-        .from("customers.cart_items")
-        .join("customers.carts", "cart_items.cart_id", "carts.cart_id")
-        .join(
-          "products.products",
-          "cart_items.product_id",
-          "products.product_id"
-        )
-        .select(
-          "cart_items.quantity",
-          "products.name",
-          "products.description",
-          "products.price",
-          "products.product_id"
-        )
-        .where("cart_items.cart_id", cart_id);
-
-      // Throw an error if the cart is empty
-      if (cartData.length === 0) {
-        throw new HttpError("Cart is empty", 400);
-      }
-
-      // Calculate the total amount
-      let total_amount = 0;
-      cartData.forEach((item) => {
-        total_amount += item.price * item.quantity;
-      });
-
-      // Query: Checkout the cart
-      const [order] = await trx("orders.orders").insert(
-        {
-          customer_id: customer_id,
-          address_id: address_id,
-          total_amount: total_amount,
-        },
-        ["*"]
-      );
-
-      // Query: Get the order ID
-      const order_id = order.order_id;
-
-      // Query: Add the cart items to the order
-      await trx("orders.order_items").insert(
-        cartData.map((item) => ({
-          order_id: order_id,
-          product_id: item.product_id,
-          quantity: item.quantity,
-          price_at_purchase: item.price,
-        }))
-      );
-
-      // Return the order data
-      return { order_details: order, order_items: cartData };
+    // Calculate the total amount
+    let total_amount = 0;
+    cartData.forEach((item) => {
+      total_amount += item.price * item.quantity;
     });
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
-  }
+
+    // Query: Checkout the cart
+    const [order] = await trx("orders.orders").insert(
+      {
+        customer_id: customer_id,
+        address_id: address_id,
+        total_amount: total_amount,
+      },
+      ["*"]
+    );
+
+    // Query: Get the order ID
+    const order_id = order.order_id;
+
+    // Query: Add the cart items to the order
+    await trx("orders.order_items").insert(
+      cartData.map((item) => ({
+        order_id: order_id,
+        product_id: item.product_id,
+        quantity: item.quantity,
+        price_at_purchase: item.price,
+      }))
+    );
+
+    // Return the order data
+    return { order_details: order, order_items: cartData };
+  });
+
+  // Return the data from the response
+  return result;
 };
 
 // Validates if a specific cart belongs to a specific user
 const validateCartIdByUserId = async (cart_id, user_id) => {
-  try {
-    // Query: Check if the cart belongs to the user
-    const [returnedData] = await knex("customers.carts")
-      .select("cart_id", "customer_id")
-      .where("cart_id", cart_id)
-      .andWhere("customer_id", user_id);
+  // Query: Check if the cart belongs to the user
+  const [returnedData] = await knex("customers.carts")
+    .select("cart_id", "customer_id")
+    .where("cart_id", cart_id)
+    .andWhere("customer_id", user_id);
 
-    // Return false if the cart doesn't belong to the user
-    if (!returnedData) {
-      return false;
-    }
-
-    // Otherwise, return true
-    return true;
-  } catch (err) {
-    // Throw an error since the query was unsuccessful
-    console.error(`Query error: ${err.message}`);
-    throw new HttpError();
+  // Return false if the cart doesn't belong to the user
+  if (!returnedData) {
+    return false;
   }
+
+  // Otherwise, return true
+  return true;
 };
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,16 @@ const orders = require("./orders");
 // JWT
 const jwt = require("../utils/jwt");
 
+// Validations
+const { validateIsUserACustomer } = require("../utils/validations/general");
+
 // Mount all routes together using this function
 const mountRoutes = (app) => {
   app.use("/auth", auth);
   app.use("/products", products);
   app.use("/users", jwt.authenticateToken, users);
-  app.use("/cart", jwt.authenticateToken, cart);
-  app.use("/orders", jwt.authenticateToken, orders);
+  app.use("/cart", jwt.authenticateToken, validateIsUserACustomer, cart);
+  app.use("/orders", jwt.authenticateToken, validateIsUserACustomer, orders);
 };
 
 module.exports = mountRoutes;
