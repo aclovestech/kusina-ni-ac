e4bdf2f505dd1b522ef4eb5cbb5470449e74f8aa
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-22 00:43:20 -0800

    chore(middleware): remove unused cart middleware to clean up the codebase chore(session.middleware.js): remove commented-out code for better readability chore(users.validator.js): remove unused customer ID validation to simplify the validator module

--- server/src/middleware/session.middleware.js
+++ server/src/middleware/session.middleware.js
@@ -1,7 +1,3 @@
-// Imports
-// const { matchedData } = require("express-validator");
-// const { isCartOwnedByCustomer } = require("../models/cart.model");
-
 exports.checkIsCustomerLoggedIn = (req, res, next) => {
   // Return an error if the user is not logged in
   if (!req.user) {
@@ -13,31 +9,3 @@ exports.checkIsCustomerLoggedIn = (req, res, next) => {
   // Move to the next middleware
   next();
 };
-
-// exports.checkIsCustomerIdTheSame = (req, res, next) => {
-//   // Get the customer ID from the validated input
-//   const { customer_id } = matchedData(req);
-
-//   // Return an error if the customer ID is not the same
-//   if (customer_id !== req.user.customer_id) {
-//     return res.status(403).json({ message: "Unauthorized" });
-//   }
-
-//   // Move to the next middleware
-//   next();
-// };
-
-// exports.checkIsCartOwnedByCustomer = async (req, res, next) => {
-//   const isOwned = await isCartOwnedByCustomer(
-//     req.session.cart_id,
-//     req.user.customer_id
-//   );
-
-//   // Return an error if the cart is not owned by the customer
-//   if (!isOwned) {
-//     return res.status(403).json({ message: "Unauthorized" });
-//   }
-
-//   // Move to the next middleware
-//   next();
-// };
