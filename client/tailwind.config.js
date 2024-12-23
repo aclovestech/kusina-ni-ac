36ee20d008319a92a5738bab4d73612f6e6fe18d
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-22 19:38:28 -0800

    feat(client): add daisyui dependency and configure it in Tailwind CSS for enhanced UI components and themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html'],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['coffee', 'dark'],
+  },
 };
