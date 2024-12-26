b138e7e8b2dc351f8162abf076cecc2021dd49b6
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-12-26 02:44:20 -0800

    feat(axiosConfig): increase timeout from 1000ms to 5000ms for better request handling and enable withCredentials for cross-origin requests

--- client/src/api/config/axiosConfig.ts
+++ client/src/api/config/axiosConfig.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const config: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 1000,
+  timeout: 5000,
+  withCredentials: true,
 };
 
 const axiosInstance: AxiosInstance = axios.create(config);
