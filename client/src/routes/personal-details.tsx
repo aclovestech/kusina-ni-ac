18aa9599105a5f017c4f3baeefa47efc26465b34
Allain Carl Mojado <ac.mojado@aclovestech.com>
2025-01-10 03:18:47 -0800

    feat(personal-details.tsx): add redirect to the home page when personal-details is being accessed

--- client/src/routes/personal-details.tsx
+++ client/src/routes/personal-details.tsx
@@ -1,6 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/personal-details')({
+  loader: async () => {
+    throw redirect({ to: '/' });
+  },
   component: PersonalDetails,
 });
 
